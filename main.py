6d2c5c88e1beb36cf76053602a1342d383427765
Harsh21-design <51110904133@piemr.edu.in>
2026-02-22 22:28:39 +0530

    add & update main file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# TASK MANAGEMENT API SYSTEM
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -17,11 +19,21 @@ def home():
 def health():
     return {"status":"OK"}
 
+# empty task list -> all tasks
+tasks = []
 
+# create a task
 @app.post("/task")
 def create_task(task: Task):
+    tasks.append(task)
     return {
         "message":"new task created successfully",
         "task": task
     }
+
+# get all task
+@app.get("/task")
+def get_all_task():
+    return {"task:":tasks}
+
 
